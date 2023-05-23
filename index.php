c90e254fbaf6ffc608b5d1f6d79de8821aacde9f
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-24 00:00:58 +0200

    struttura + api

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+     <title>php-dischi-json</title>
+</head>
+<body>
+
+     <div id="app">
+
+          <div class="vh-100 bg-secondary">
+
+          </div>
+
+     </div>
+     
+
+     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+     <script src="./main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+     api: 'server.php',
+     data: '',
+    }
+  },
+
+  methods: {
+     chiamataApi(){
+          axios.get(this.api)
+               .then((res) => {
+                    this.data = res.data;
+               });
+     },
+  },
+
+  mounted(){
+     this.chiamataApi();
+  },
+
+}).mount('#app')
